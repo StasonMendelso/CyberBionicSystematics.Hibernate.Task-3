722fb3b1756e8cb2ab0db5cc60016def11bad8ee
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-29 19:17:11 +0300

    initial commit

---
+++ src/main/java/org/stanislav/App.java
@@ -0,0 +1,13 @@
+package org.stanislav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
